45dfd1b25b10ccdba5b478a253835f9190682cab
blrindian <blrindian100@gmail.com>
2024-03-21 16:03:13 -0400

    hello.py file updated

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello GVG")
+name = 'GV'
+say = "Hello"
+
+print(say, name)
+
+wiki = '''Save your favorite articles to read offline,
+sync your reading lists across devices and customize
+your reading experience with the official Wikipedia app.'''
+
+print(wiki)
